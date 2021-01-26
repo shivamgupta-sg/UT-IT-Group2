8a066c0047cb99a4f0106070774454f0094024e1
shivamgupta-sg <shivam.gupta@knoldus.com>
2021-01-26 14:49:07 +0530

    added implementation for EmailValidator

--- ques3/src/main/scala/com/knoldus/validator/EmailValidator.scala
+++ ques3/src/main/scala/com/knoldus/validator/EmailValidator.scala
@@ -1,5 +1,12 @@
 package com.knoldus.validator
 
 class EmailValidator {
-  def emailIdIsValid(emailId: String): Boolean = ???
+  def emailIdIsValid(emailId: String): Boolean = {
+    val emailIdRegex = "/[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}"
+
+    if (!emailId.matches(emailIdRegex))
+      return false
+
+    true
+  }
 }
