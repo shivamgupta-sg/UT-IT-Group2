98b66c94abf96691bed5c7c58de5a13a5852971f
shivamgupta-sg <shivam.gupta@knoldus.com>
2021-01-26 13:37:15 +0530

    added tests for PasswordValidator

---
+++ ques1&2/src/test/scala/com/knoldus/validator/PasswordValidatorTest.scala
@@ -0,0 +1,20 @@
+package com.knoldus.validator
+
+import org.scalatest.flatspec.AnyFlatSpec
+
+class PasswordValidatorTest extends AnyFlatSpec {
+  "Password" should "be valid" in {
+    val passwordValidator = new PasswordValidator()
+
+    val result = passwordValidator.isValid("Abcd@1234")
+
+    assert(result)
+  }
+
+  it should "not be valid" in {
+    val passwordValidator = new PasswordValidator()
+
+    val result = passwordValidator.isValid("abcd@1234")
+    assert(!result)
+  }
+}
