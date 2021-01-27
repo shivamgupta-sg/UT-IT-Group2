eacfe143686599a8bf18c0fe49aad81031ce87b9
shivamgupta-sg <shivam.gupta@knoldus.com>
2021-01-27 16:26:15 +0530

    create tests for UserValidator

---
+++ ques3/src/test/scala/com/knoldus/validator/UserValidatorTest.scala
@@ -0,0 +1,58 @@
+package com.knoldus.validator
+
+import com.knoldus.models.User
+import org.scalatest.flatspec.AnyFlatSpec
+
+class UserValidatorTest extends AnyFlatSpec {
+
+  behavior of "UserValidatorTest"
+
+  it should "be valid as company exists and email is valid" in {
+    val userValidator = new UserValidator()
+
+    val userInputUser: User = User("Shivam",
+      "shivam@knoldus.com",
+      "Knoldus")
+
+    val result = userValidator.isValid(userInputUser)
+
+    assert(result)
+  }
+
+  it should "not be valid as email is not valid" in {
+    val userValidator = new UserValidator()
+
+    val userInputUser: User = User("Shivam",
+      "shivam$123@sony.com",
+      "Philips")
+
+    val result = userValidator.isValid(userInputUser)
+
+    assert(!result)
+  }
+
+  it should "not be valid as company doesn't exists" in {
+    val userValidator = new UserValidator()
+
+    val userInputUser: User = User("Shivam",
+      "shivam.123@sony.com",
+      "Sony")
+
+    val result = userValidator.isValid(userInputUser)
+
+    assert(!result)
+  }
+
+  it should "not be valid as email is not valid and company doesn't exists" in {
+    val userValidator = new UserValidator()
+
+    val userInputUser: User = User("Shivam",
+      "shivam$123@sony.com",
+      "Sony")
+
+    val result = userValidator.isValid(userInputUser)
+
+    assert(!result)
+  }
+
+}
