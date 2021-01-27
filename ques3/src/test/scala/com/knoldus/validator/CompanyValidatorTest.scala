02aaada8e9f65d9cd12b2d6ea6acbd791b778156
shivamgupta-sg <shivam.gupta@knoldus.com>
2021-01-27 17:12:51 +0530

    added some more tests

--- ques3/src/test/scala/com/knoldus/validator/CompanyValidatorTest.scala
+++ ques3/src/test/scala/com/knoldus/validator/CompanyValidatorTest.scala
@@ -17,11 +17,35 @@ class CompanyValidatorTest extends AnyFlatSpec {
     assert(result)
   }
 
-  it should "not be valid" in {
+  it should "not be valid as email is not valid" in {
     val companyValidator = new CompanyValidator()
 
     val userInputComapny: Company = Company("Sony",
       "sony$123@sony.com",
+      "Noida")
+
+    val result = companyValidator.companyIsValid(userInputComapny)
+
+    assert(!result)
+  }
+
+  it should "not be valid as company exists" in {
+    val companyValidator = new CompanyValidator()
+
+    val userInputComapny: Company = Company("Philips",
+      "phil.123@philips.com",
+      "Noida")
+
+    val result = companyValidator.companyIsValid(userInputComapny)
+
+    assert(!result)
+  }
+
+  it should "not be valid as email is not valid and company exists" in {
+    val companyValidator = new CompanyValidator()
+
+    val userInputComapny: Company = Company("Philips",
+      "phil$123@philips.com",
       "Noida")
 
     val result = companyValidator.companyIsValid(userInputComapny)
